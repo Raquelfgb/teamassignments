8fe2112db3c73231dc42c2bcf6283e1da396f17c
Skyler <sjskyler27@gmail.com>
2023-04-21 12:32:50 -0600

    added function clear local storage to utils

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,11 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+// clear local storage
+export function clearLocalStorage(key, data) {
+  localStorage.clear();
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
