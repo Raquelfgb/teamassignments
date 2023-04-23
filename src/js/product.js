0904db81ad596bf7c613cbd7f07eb060efb6d488
alissoninacio1 <alissoninacio96@gmail.com>
2023-04-22 18:08:49 -0700

    cart issue set

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,31 @@
 import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage } from './utils.mjs';
+//import { clearLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
+
+// create product object using productdata.mjs
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
-}
-// add to cart button event handler
+// add to cart button event handler, called on by event listener 
 async function addToCartHandler(e) {
+  //e refers to the event, click that was passed through then gets the target, in this case the add button
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
+
+function addProductToCart(product) {
+  //modification to add, rather then set.
+  const cart = getLocalStorage('so-cart') || []; // retrieve current cart data or create a new empty array if it doesn't exist
+  cart.push(product); // add new product to cart array
+  setLocalStorage('so-cart', cart); // save updated cart array to local storage
+
+  //clearLocalStorage(); 
+}
+
+
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+.getElementById('addToCart')
+.addEventListener('click', addToCartHandler);
