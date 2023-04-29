ee2de73641425e8fc57328d88f5ebc0ddaf25143
chigham <chigham@byui.edu>
2023-04-29 15:02:51 -0600

    First 4 steps of the activity.

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+const productList = new ProductListing("tents", dataSource, "")
