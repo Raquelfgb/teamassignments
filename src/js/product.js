b657924b68cb3d4e474fd5d3dbacafcb5f679b00
chigham <chigham@byui.edu>
2023-04-29 13:29:07 -0600

    Set to match solution

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,14 @@
 import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
-import { setLocalStorage, getParam } from './utils.mjs';
+import { setLocalStorage, getParams } from './utils.mjs';
 
-const productId = getParam('product');
+const productId = getParams('product');
 const dataSource = new ProductData('tents');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-// record it to the console
-console.log(dataSource)
-console.log(productId)
+/*// record it to the console
 console.log(dataSource.findProductById(productId));
 
 // add to cart button event handler, called on by event listener 
@@ -43,6 +41,6 @@ function addProductToCart(product) {
 // add listener to Add to Cart button
 document
 .getElementById("addToCart")
-.addEventListener("click", addToCartHandler);
+.addEventListener("click", addToCartHandler);*/
 
 
