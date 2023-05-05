fe446d8c2b14c8fb63efb8a9639a625793d95126
alissoninacio1 <alissoninacio96@gmail.com>
2023-05-05 12:41:39 -0700

    alert.js and alert.json created

---
+++ src/js/Alert.js
@@ -0,0 +1,9 @@
+//Create an Alert.js file (module). 
+//In the file add a class called Alert and export it as default.
+export default class Alert {
+
+
+    constructor(alertMessage) {
+        this.alertMessage = alertMessage
+    }
+}
