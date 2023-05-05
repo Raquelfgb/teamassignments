49c7a78df58a48d43720d634e07260709f559831
Melody <cur21014@byui.edu>
2023-05-04 20:33:39 -0600

    added up to step where you'll add 4 grids for the category icons

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+// import ProductData from "./ProductData.mjs";
+// import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData('tents');
-const element = document.getElementById("productList");
+// const dataSource = new ProductData('tents');
+// const element = document.getElementById("productList");
 
-const productList = new ProductList("Tents", dataSource, element);
+// const productList = new ProductList("Tents", dataSource, element);
 
-productList.init();
+// productList.init();
 
 
 
