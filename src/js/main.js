38e0e346df385e36801ca37c1832012ec6e2cc6c
Melody <cur21014@byui.edu>
2023-05-05 20:27:31 -0600

    moved some things into their proper places

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 // import ProductData from "./ProductData.mjs";
 // import ProductList from "./ProductList.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+// import ProductData from "./ProductData.mjs";
+// import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
@@ -12,8 +12,3 @@ loadHeaderFooter();
 // const productList = new ProductList("Tents", dataSource, element);
 
 // productList.init();
-
-
-
-
-
