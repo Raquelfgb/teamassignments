0ca005d8811f32595341d84fbf392530e1dc7a36
Skyler Simpson <sjskyler27@gmail.com>
2023-05-07 21:39:48 -0600

    reconfigured produc_listings html

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,5 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const element = document.getElementById("productList");
@@ -7,3 +8,4 @@ const element = document.getElementById("productList");
 const productList = new ProductListing("Tents", dataSource, element);
 
 productList.init();
+loadHeaderFooter();
