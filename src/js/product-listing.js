c3ac532b713108a9a6a2766edf06b30927247ec9
Skyler Simpson <sjskyler27@gmail.com>
2023-05-07 22:07:47 -0600

    added hyperlinks with parameters to index, fixed css and added headers to product grid

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,6 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 const element = document.getElementById("productList");
@@ -8,4 +9,4 @@ const element = document.getElementById("productList");
 const productList = new ProductListing("Tents", dataSource, element);
 
 productList.init();
-loadHeaderFooter();
+
