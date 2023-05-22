1300bd7c7be71cadb6f909e581cb26b0980a706b
chigham <chigham@byui.edu>
2023-05-21 22:07:54 -0600

    vite change

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,8 @@ export default defineConfig({
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         checkedout: resolve(__dirname, "src/checkout/success.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        listing: resolve(__dirname, "src/product_listing/index.html")
+        product_pages: resolve(__dirname, "src/product_pages/index.html"),
+        product_listing: resolve(__dirname, "src/product_listing/index.html")
         /*main: resolve(__dirname, "src/js/main.js"),
         cart: resolve(__dirname, "src/js/cart.js"),
         checkout: resolve(__dirname, "src/js/checkout.js"),
