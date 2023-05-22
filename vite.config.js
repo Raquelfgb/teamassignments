b53e7dd1d121ed0f2edf865463ae9efa9d687730
chigham <chigham@byui.edu>
2023-05-21 21:55:44 -0600

    Vite update.

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,17 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        /*main: resolve(__dirname, "src/index.html"),
+        main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+        checkedout: resolve(__dirname, "src/checkout/success.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
-        listing: resolve(__dirname, "src/product_listing/index.html")*/
-        main: resolve(__dirname, "src/js/main.js"),
+        listing: resolve(__dirname, "src/product_listing/index.html")
+        /*main: resolve(__dirname, "src/js/main.js"),
         cart: resolve(__dirname, "src/js/cart.js"),
         checkout: resolve(__dirname, "src/js/checkout.js"),
         product_pages: resolve(__dirname, "src/js/product.js"),
-        product_listing: resolve(__dirname, "src/js/product-listing.js")
+        product_listing: resolve(__dirname, "src/js/product-listing.js")*/
       },
     },
   },
